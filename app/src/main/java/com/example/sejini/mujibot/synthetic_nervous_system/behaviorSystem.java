3ad60740e3e154b8bd1906406e0c4c1aca6e7f74
sefide <sefide@naver.com>
2016-12-17 20:42:46 +0900

    파일 구조 설계

---
+++ app/src/main/java/com/example/sejini/mujibot/synthetic_nervous_system/behaviorSystem.java
@@ -0,0 +1,8 @@
+package com.example.sejini.mujibot.synthetic_nervous_system;
+
+/**
+ * Created by dahee on 2016-12-17.
+ */
+
+public class behaviorSystem {
+}
---
+++ app/src/main/java/com/example/sejini/mujibot/synthetic_nervous_system/motivationSystem.java
@@ -0,0 +1,11 @@
+package com.example.sejini.mujibot.synthetic_nervous_system;
+
+/**
+ * Created by dahee on 2016-12-17.
+ */
+
+public class motivationSystem {
+
+}
+
+
