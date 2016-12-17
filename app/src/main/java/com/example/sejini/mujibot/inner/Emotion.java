9b31d732b8b790bd37a5578582431d98af7de244
se-jin son <thstpwlsq@gmail.com>
2016-12-17 22:29:33 +0900

    inner package의 Emotion System class 작성

--- app/src/main/java/com/example/sejini/mujibot/inner/Emotion.java
+++ app/src/main/java/com/example/sejini/mujibot/inner/Emotion.java
@@ -14,4 +14,88 @@ public class Emotion {
     private int FEAR=50;
     private int DISGUST=50;
     private int ANGER=50;
+
+    public void init(){
+        JOY=50;
+        INTEREST=50;
+        BOREDOM=50;
+        SURPRISE=50;
+        CALM=50;
+        SORROW=50;
+        FEAR=50;
+        DISGUST=50;
+        ANGER=50;
+    }
+
+    public int getJOY() {
+        return JOY;
+    }
+
+    public void setJOY(int JOY) {
+        this.JOY = JOY;
+    }
+
+    public int getINTEREST() {
+        return INTEREST;
+    }
+
+    public void setINTEREST(int INTEREST) {
+        this.INTEREST = INTEREST;
+    }
+
+    public int getBOREDOM() {
+        return BOREDOM;
+    }
+
+    public void setBOREDOM(int BOREDOM) {
+        this.BOREDOM = BOREDOM;
+    }
+
+    public int getSURPRISE() {
+        return SURPRISE;
+    }
+
+    public void setSURPRISE(int SURPRISE) {
+        this.SURPRISE = SURPRISE;
+    }
+
+    public int getCALM() {
+        return CALM;
+    }
+
+    public void setCALM(int CALM) {
+        this.CALM = CALM;
+    }
+
+    public int getSORROW() {
+        return SORROW;
+    }
+
+    public void setSORROW(int SORROW) {
+        this.SORROW = SORROW;
+    }
+
+    public int getFEAR() {
+        return FEAR;
+    }
+
+    public void setFEAR(int FEAR) {
+        this.FEAR = FEAR;
+    }
+
+    public int getDISGUST() {
+        return DISGUST;
+    }
+
+    public void setDISGUST(int DISGUST) {
+        this.DISGUST = DISGUST;
+    }
+
+    public int getANGER() {
+        return ANGER;
+    }
+
+    public void setANGER(int ANGER) {
+        this.ANGER = ANGER;
+    }
 }
