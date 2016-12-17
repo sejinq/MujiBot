2005d781f97403f5dc786764f5bf1127872914d3
se-jin son <thstpwlsq@gmail.com>
2016-12-17 20:35:07 +0900

    package추가

---
+++ app/src/main/java/com/example/sejini/mujibot/inner/Emotion.java
@@ -0,0 +1,17 @@
+package com.example.sejini.mujibot.inner;
+
+/**
+ * Created by sejini on 2016-12-17.
+ */
+
+public class Emotion {
+    private int JOY=50;
+    private int INTEREST=50;
+    private int BOREDOM=50;
+    private int SURPRISE=50;
+    private int CALM=50;
+    private int SORROW=50;
+    private int FEAR=50;
+    private int DISGUST=50;
+    private int ANGER=50;
+}
