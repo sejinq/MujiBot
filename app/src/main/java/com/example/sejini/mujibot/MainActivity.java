e2bf6726a0f3ecc9ae2e0704178276021ea50ee2
sefide <sefide@naver.com>
2016-12-18 16:02:24 +0900

    mainActivity 버튼 선언

--- app/src/main/java/com/example/sejini/mujibot/MainActivity.java
+++ app/src/main/java/com/example/sejini/mujibot/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.sejini.mujibot;
 
-import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    boolean is_PICTURE_LIKE = false;
+    boolean is_PICTURE_DISLIKE = false;
+    boolean is_PICTURE_TREATMENT = false;
+    boolean is_EEG_HAPPY = false;
+    boolean is_EEG_SORROW = false;
+    boolean is_EEG_ANGER = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button PICTURE_LIKE = (Button)findViewById(R.id.btn_picture_like);
+        Button PICTURE_DISLIKE = (Button)findViewById(R.id.btn_picture_dislike);
+        Button PICTURE_TREATMENT = (Button)findViewById(R.id.btn_picture_treatment);
+        Button EEG_HAPPY = (Button)findViewById(R.id.btn_eeg_happy);
+        Button EEG_SORROW = (Button)findViewById(R.id.btn_eeg_sorrow);
+        Button EEG_ANGER = (Button)findViewById(R.id.btn_eeg_anger);
 
     }
 }
--- app/src/main/java/com/example/sejini/mujibot/inner/PhysiologicalNeed.java
+++ app/src/main/java/com/example/sejini/mujibot/inner/PhysiologicalNeed.java
@@ -1,11 +1,20 @@
 package com.example.sejini.mujibot.inner;
 
-import java.util.GregorianCalendar;
+import java.util.Calendar;
 
 /**
  * Created by sejini on 2016-12-18.
  */
 
 public class PhysiologicalNeed {
+    private final Calendar c = Calendar.getInstance();
+    private Calendar calSet = (Calendar) c.clone();
+
+//    calSet.set(Calendar.HOUR_OF_DAY, 10);
+//    calSet.set(Calendar.MINUTE, 2);
+//    calSet.set(Calendar.SECOND, 0);
+//    calSet.set(Calendar.MILLISECOND, 0);
 
 }
+//버튼이 온이 되었을 때 start시간 시작
+//어플 시작할 때 start타임 시작 main 에서 체크 현재 시간 비교 4시간이
