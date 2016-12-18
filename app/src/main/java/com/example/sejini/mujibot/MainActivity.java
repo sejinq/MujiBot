f43957cb319e568b015458328aeea7e6b25344d3
sefide <sefide@naver.com>
2016-12-18 15:20:57 +0900

    perceptionSystem 수정 - 함수로 gain값을 inner 감정에 더해준다

--- app/src/main/java/com/example/sejini/mujibot/MainActivity.java
+++ app/src/main/java/com/example/sejini/mujibot/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.sejini.mujibot;
 
-import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
     }
--- app/src/main/java/com/example/sejini/mujibot/synthetic_nervous_system/perceptionSystem.java
+++ app/src/main/java/com/example/sejini/mujibot/synthetic_nervous_system/perceptionSystem.java
@@ -1,10 +1,117 @@
 package com.example.sejini.mujibot.synthetic_nervous_system;
 //perceptionSystem -
+
+import com.example.sejini.mujibot.inner.Emotion;
+
 /**
  * Created by dahee on 2016-12-17.
  */
 
 public class perceptionSystem {
+
+    //( JOY/BOREDOM/INTEREST/SURPRISE/SORROW/CALM/FEAR/DISGUST/ANGER)
+    public void showPictureLike(){
+        Emotion.JOY += 0.5;
+        Emotion.INTEREST += 0.5;
+        Emotion.BOREDOM -= 0.3;
+        Emotion.SURPRISE += 0.1;
+        Emotion.CALM -= 0.1;
+        Emotion.SORROW -= 0.3;
+        Emotion.FEAR -= 0.3;
+        Emotion.DISGUST -= 0.2;
+        Emotion.ANGER -= 0.3;
+
+    }
+
+    public void showPictureDislike(){
+        Emotion.JOY -= 0.3;
+        Emotion.INTEREST -= 0.3;
+        Emotion.BOREDOM += 0.3;
+        Emotion.SURPRISE -= 0.1;
+        Emotion.CALM += 0.1;
+        Emotion.SORROW += 0.2;
+        Emotion.FEAR += 0.2;
+        Emotion.DISGUST += 0.5;
+        Emotion.ANGER += 0.5;
+
+    }
+
+    public void showPictureTreatment(){
+        Emotion.JOY += 0.1;
+        Emotion.INTEREST += 0.1;
+        Emotion.BOREDOM -= 0.1;
+        Emotion.SURPRISE -= 0.1;
+        Emotion.CALM += 0.3;
+        Emotion.SORROW -= 0.1;
+        Emotion.FEAR -= 0.1;
+        Emotion.DISGUST -= 0.1;
+        Emotion.ANGER -= 0.1;
+
+    }
+    public void checkEEGhappy(){
+        Emotion.JOY += 0.5;
+        Emotion.INTEREST += 0.1;
+        Emotion.BOREDOM -= 0.1;
+        Emotion.SURPRISE += 0.1;
+        Emotion.CALM -= 0.1;
+        Emotion.SORROW -= 0.5;
+        Emotion.FEAR -= 0.1;
+        Emotion.DISGUST -= 0.1;
+        Emotion.ANGER -= 0.2;
+
+    }
+
+    public void checkEEGsorrow(){
+        Emotion.JOY -= 0.5;
+        Emotion.INTEREST += 0.1;
+        Emotion.BOREDOM -= 0.1;
+        Emotion.SURPRISE += 0.1;
+        Emotion.CALM -= 0.1;
+        Emotion.SORROW += 0.5;
+        Emotion.FEAR += 0.1;
+        Emotion.DISGUST += 0.1;
+        Emotion.ANGER += 0.2;
+
+    }
+
+    public void checkEEGanger(){
+        Emotion.JOY -= 0.4;
+        Emotion.INTEREST += 0.1;
+        Emotion.BOREDOM -= 0.1;
+        Emotion.SURPRISE += 0.5;
+        Emotion.CALM -= 0.1;
+        Emotion.SORROW += 0.2;
+        Emotion.FEAR += 0.1;
+        Emotion.DISGUST += 0.1;
+        Emotion.ANGER += 0.5;
+
+    }
+
+    public void hitMujibot(){
+        Emotion.JOY -= 0.2;
+        Emotion.INTEREST += 0.1;
+        Emotion.BOREDOM -= 0.1;
+        Emotion.SURPRISE += 0.5;
+        Emotion.CALM -= 0.2;
+        Emotion.SORROW += 0.2;
+        Emotion.FEAR += 0.5;
+        Emotion.DISGUST += 0.2;
+        Emotion.ANGER += 0.5;
+
+    }
+
+    public void petMujibot(){
+        Emotion.JOY += 0.2;
+        Emotion.INTEREST += 0.5;
+        Emotion.BOREDOM -= 0.5;
+        Emotion.SURPRISE += 0.2;
+        Emotion.CALM -= 0.1;
+        Emotion.SORROW -= 0.2;
+        Emotion.FEAR += 0.1;
+        Emotion.DISGUST -= 0.1;
+        Emotion.ANGER -= 0.1;
+
+    }
 
 
 }
