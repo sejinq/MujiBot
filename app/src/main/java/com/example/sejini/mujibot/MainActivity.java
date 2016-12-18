e7082b0875aec72dc97b9a2953bfa47a699b20a9
se-jin son <thstpwlsq@gmail.com>
2016-12-18 16:48:57 +0900

    behavior system 구현 완료 main.xml 수정

--- app/src/main/java/com/example/sejini/mujibot/MainActivity.java
+++ app/src/main/java/com/example/sejini/mujibot/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sejini.mujibot;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     boolean is_PICTURE_LIKE = false;
@@ -11,12 +12,13 @@ public class MainActivity extends AppCompatActivity {
     boolean is_EEG_HAPPY = false;
     boolean is_EEG_SORROW = false;
     boolean is_EEG_ANGER = false;
-
+    public static ImageView mujiBot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mujiBot = (ImageView)findViewById(R.id.mujibot);
         Button PICTURE_LIKE = (Button)findViewById(R.id.btn_picture_like);
         Button PICTURE_DISLIKE = (Button)findViewById(R.id.btn_picture_dislike);
         Button PICTURE_TREATMENT = (Button)findViewById(R.id.btn_picture_treatment);
--- app/src/main/java/com/example/sejini/mujibot/synthetic_nervous_system/behaviorSystem.java
+++ app/src/main/java/com/example/sejini/mujibot/synthetic_nervous_system/behaviorSystem.java
@@ -1,6 +1,10 @@
 package com.example.sejini.mujibot.synthetic_nervous_system;
 
+import com.example.sejini.mujibot.MainActivity;
+import com.example.sejini.mujibot.R;
 import com.example.sejini.mujibot.inner.Emotion;
+
+import java.util.Random;
 
 /**
  * Created by dahee on 2016-12-17.
@@ -8,63 +12,155 @@ import com.example.sejini.mujibot.inner.Emotion;
 
 public class behaviorSystem {
     public static void checkEmotion(int emotion){
+        Random random = new Random();
+        int randomValue = random.nextInt(3);
+
         if(emotion == Emotion.feelJOY){
-            JOYBehavior();
+            JOYBehavior(randomValue);
         }
         else if(emotion == Emotion.feelINTEREST){
-            INTERESTBehavior();
+            INTERESTBehavior(randomValue);
         }
         else if(emotion == Emotion.feelANGER){
-            ANGERBehavior();
+            ANGERBehavior(randomValue);
         }
         else if(emotion == Emotion.feelBOREDOM){
-            BOREDOMBehavior();
+            BOREDOMBehavior(randomValue);
         }
         else if(emotion == Emotion.feelCALM){
-            CALMBehavior();
+            CALMBehavior(randomValue);
         }
         else if(emotion == Emotion.feelDISGUST){
-            DISGUSTBehavior();
+            DISGUSTBehavior(randomValue);
         }
         else if(emotion == Emotion.feelFEAR){
-            FEARBehavior();
+            FEARBehavior(randomValue);
         }
         else if(emotion == Emotion.feelSORROW){
-            SORROWBehavior();
+            SORROWBehavior(randomValue);
         }
         else if(emotion == Emotion.feelSURPRISE){
-            SURPRISEBehavior();
+            SURPRISEBehavior(randomValue);
         }
         else{
             return;
         }
     }
 
-    private static void DISGUSTBehavior() {
-
+    private static void DISGUSTBehavior(int randomValue) {
+        switch (randomValue){
+            case 0:
+                MainActivity.mujiBot.setImageResource(R.drawable.disgust1);break;
+            case 1:
+                MainActivity.mujiBot.setImageResource(R.drawable.disgust2);break;
+            case 2:
+                MainActivity.mujiBot.setImageResource(R.drawable.disgust3);break;
+            default:
+                MainActivity.mujiBot.setImageResource(R.drawable.normal1);
+        }
     }
 
-    private static void CALMBehavior() {
+    private static void CALMBehavior(int randomValue) {
+        switch (randomValue){
+            case 0:
+                MainActivity.mujiBot.setImageResource(R.drawable.calm1);break;
+            case 1:
+                MainActivity.mujiBot.setImageResource(R.drawable.calm2);break;
+            case 2:
+                MainActivity.mujiBot.setImageResource(R.drawable.calm3);break;
+            default:
+                MainActivity.mujiBot.setImageResource(R.drawable.normal1);
+        }
     }
 
-    private static void FEARBehavior() {
+    private static void FEARBehavior(int randomValue) {
+        switch (randomValue){
+            case 0:
+                MainActivity.mujiBot.setImageResource(R.drawable.fear1);break;
+            case 1:
+                MainActivity.mujiBot.setImageResource(R.drawable.fear2);break;
+            case 2:
+                MainActivity.mujiBot.setImageResource(R.drawable.fear3);break;
+            default:
+                MainActivity.mujiBot.setImageResource(R.drawable.normal1);
+        }
     }
 
-    private static void SORROWBehavior() {
+    private static void SORROWBehavior(int randomValue) {
+        switch (randomValue){
+            case 0:
+                MainActivity.mujiBot.setImageResource(R.drawable.sorrow1);break;
+            case 1:
+                MainActivity.mujiBot.setImageResource(R.drawable.sorrow2);break;
+            case 2:
+                MainActivity.mujiBot.setImageResource(R.drawable.sorrow3);break;
+            default:
+                MainActivity.mujiBot.setImageResource(R.drawable.normal1);
+        }
     }
 
-    private static void SURPRISEBehavior() {
+    private static void SURPRISEBehavior(int randomValue) {
+        switch (randomValue){
+            case 0:
+                MainActivity.mujiBot.setImageResource(R.drawable.surprise);break;
+            case 1:
+                MainActivity.mujiBot.setImageResource(R.drawable.surprise2);break;
+            case 2:
+                MainActivity.mujiBot.setImageResource(R.drawable.surprise3);break;
+            default:
+                MainActivity.mujiBot.setImageResource(R.drawable.normal1);
+        }
     }
 
-    private static void BOREDOMBehavior() {
+    private static void BOREDOMBehavior(int randomValue) {
+        switch (randomValue){
+            case 0:
+                MainActivity.mujiBot.setImageResource(R.drawable.boredom1);break;
+            case 1:
+                MainActivity.mujiBot.setImageResource(R.drawable.boredom2);break;
+            case 2:
+                MainActivity.mujiBot.setImageResource(R.drawable.boredom3);break;
+            default:
+                MainActivity.mujiBot.setImageResource(R.drawable.normal1);
+        }
     }
 
-    private static void ANGERBehavior() {
+    private static void ANGERBehavior(int randomValue) {
+        switch (randomValue){
+            case 0:
+                MainActivity.mujiBot.setImageResource(R.drawable.anger1);break;
+            case 1:
+                MainActivity.mujiBot.setImageResource(R.drawable.anger2);break;
+            case 2:
+                MainActivity.mujiBot.setImageResource(R.drawable.anger3);break;
+            default:
+                MainActivity.mujiBot.setImageResource(R.drawable.normal1);
+        }
     }
 
-    private static void INTERESTBehavior() {
+    private static void INTERESTBehavior(int randomValue) {
+        switch (randomValue){
+            case 0:
+                MainActivity.mujiBot.setImageResource(R.drawable.interest1);break;
+            case 1:
+                MainActivity.mujiBot.setImageResource(R.drawable.interest2);break;
+            case 2:
+                MainActivity.mujiBot.setImageResource(R.drawable.interest3);break;
+            default:
+                MainActivity.mujiBot.setImageResource(R.drawable.normal1);
+        }
     }
 
-    private static void JOYBehavior() {
+    private static void JOYBehavior(int randomValue) {
+        switch (randomValue){
+            case 0:
+                MainActivity.mujiBot.setImageResource(R.drawable.joy1);break;
+            case 1:
+                MainActivity.mujiBot.setImageResource(R.drawable.joy2);break;
+            case 2:
+                MainActivity.mujiBot.setImageResource(R.drawable.joy3);break;
+            default:
+                MainActivity.mujiBot.setImageResource(R.drawable.normal1);
+        }
     }
 }
